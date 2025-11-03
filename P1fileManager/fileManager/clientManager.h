8a5421db8141001a7a00ea0a94e71a154debc5b9
Rodrigo Fernandez <rodrixfc17@gmail.com>
2025-11-03 14:51:49 +0100

    mini push

--- P1fileManager/fileManager/clientManager.h
+++ P1fileManager/fileManager/clientManager.h
@@ -12,3 +12,9 @@ typedef enum {
     ack
 }msgTypes;
 
+class clientManager {
+    static inline map<FileManager*, int> connectionIds;
+    static inline map<int, FileManager> instanciasFileManager;
+
+    static void atiendeCliente(int clientId);
+};
