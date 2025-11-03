3cde263d106f47c8152967ecd1e34a61caa46e2d
Rodrigo Fernandez <rodrixfc17@gmail.com>
2025-11-03 14:14:25 +0100

    mini push

---
+++ P1fileManager/fileManager/clientManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Rodrigo on 03/11/2025.
+//
+
+#include "clientManager.h"
---
+++ P1fileManager/fileManager/clientManager.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "utils.h"
+#include <string>
+#include "filemanager.h"
+
+using namespace std;
+
+typedef enum {
+    constructorFilemanager,
+    constructorFilemanagerParams,
+    ack
+}msgTypes;
+
